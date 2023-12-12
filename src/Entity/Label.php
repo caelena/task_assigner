757195586e97566e3011b87541bcb64b5e351b02
mmiradev <mmirgal868@g.educaand.es>
2023-12-12 14:36:10 +0100

    added ORM entity to Label.php

--- src/Entity/Label.php
+++ src/Entity/Label.php
@@ -2,11 +2,21 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'label')]
 class Label
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
     private ?int $id;
+    #[ORM\Column(type: 'string')]
     private ?string $description;
+    #[ORM\Column(type: 'string', unique: true)]
     private ?string $abbreviation;
+    #[ORM\Column(type: 'boolean')]
     private ?bool $archived;
 
     public function getId(): ?int
