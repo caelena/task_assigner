be71b804533b66c1f8f88c3888e628262d6cfe42
mmiradev <mmirgal868@g.educaand.es>
2023-12-12 14:31:58 +0100

    added ORM entity to Task.php

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -2,14 +2,27 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'task')]
 class Task
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
     private ?int $id;
+    #[ORM\Column(type: 'string')]
     private ?string $title;
+    #[ORM\Column(type: 'string')]
     private ?Employee $employee;
+    #[ORM\Column(type: 'integer')]
     private ?int $priority;
+    #[ORM\Column(type: 'string', unique: true)]
     private ?string $code;
+    #[ORM\Column(type: 'time', nullable: true)]
     private ?\DateTimeInterface $estimatedTime;
+    #[ORM\Column(type: 'text')]
     private ?string $details;
 
     public function getId(): ?int
