ab6fd320b4a17643bae904b4e675c5f835abad6b
mmiradev <mmirgal868@g.educaand.es>
2024-01-17 13:27:53 +0100

    created Priority class

---
+++ src/Entity/Priority.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'priority')]
+class Priority
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+    #[ORM\Column(type: 'integer')]
+    private int $number;
+    #[ORM\Column(type: 'string')]
+    private string $abbreviation;
+    #[ORM\Column(type: 'string', length: 80)]
+    private string $description;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNumber(): int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): Priority
+    {
+        $this->number = $number;
+        return $this;
+    }
+
+    public function getAbbreviation(): string
+    {
+        return $this->abbreviation;
+    }
+
+    public function setAbbreviation(string $abbreviation): Priority
+    {
+        $this->abbreviation = $abbreviation;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): Priority
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+
+}
