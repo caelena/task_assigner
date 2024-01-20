65785f949540dfc2e96450a0be36ac135c359f3d
mmiradev <mmirgal868@g.educaand.es>
2024-01-20 15:59:35 +0100

    created ArchivedLabel class

---
+++ src/Entity/ArchivedLabel.php
@@ -0,0 +1,22 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'archived_label')]
+class ArchivedLabel extends Label
+{
+    #[ORM\Column(type: 'date')]
+    private ?\DateTimeInterface $archiving_date;
+
+    public function getArchivingDate(): ?DateTimeInterface
+    {
+        return $this->archiving_date;
+    }
+
+    public function setArchivingDate(?DateTimeInterface $archiving_date): ArchivedLabel
+    {
+        $this->archiving_date = $archiving_date;
+        return $this;
+    }
+}
