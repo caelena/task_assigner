b97e1d04070d00125bb7dd08dd245d74d6035c6d
mmiradev <mmirgal868@g.educaand.es>
2024-01-30 13:03:40 +0100

    created controller

---
+++ src/Controller/TaskAssignerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EmployeeRepository;
+use App\Repository\LabelRepository;
+use App\Repository\TaskRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskAssignerController extends AbstractController
+{
+
+}
