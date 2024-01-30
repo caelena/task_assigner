345aa6b29cb47bfa642c096fe0e09d4af24c7441
mmiradev <mmirgal868@g.educaand.es>
2024-01-30 13:39:33 +0100

    created controller for listing tasks

--- src/Controller/TaskAssignerController.php
+++ src/Controller/TaskAssignerController.php
@@ -11,5 +11,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TaskAssignerController extends AbstractController
 {
+    #[Route('/tarea/listado', name: 'tarea_listar',)]
+    public function tareaListar(TaskRepository $taskRepository): Response
+    {
+        $tasks = $taskRepository->findBy([], ['title'=>'ASC']);
+        return $this->render('tarealistado.html.twig', [
+            'tasks' => $tasks
+        ]);
+    }
 
+    #[Route('/empleado/listar', name: 'empleado_listar')]
+    public function empleadoListar(EmployeeRepository $employeeRepository): Response
+    {
+
+        return $this->render('');
+    }
+
+    #[Route('/empleado/nombre/{nombre}', name: "empleado_filtrar", requirements: ['nombre' => '\w+'])]
+    public function empleadoFiltrar(EmployeeRepository $employeeRepository, string $nombre): Response
+    {
+
+        return $this->render('');
+    }
+
+    #[Route('/etiqueta/listar', name: 'etiqueta_listar')]
+    public function etiquetaListar(LabelRepository $labelRepository): Response
+    {
+
+        return $this->render('');
+    }
 }
