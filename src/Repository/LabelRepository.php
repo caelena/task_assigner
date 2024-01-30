89a95aafff8f6e6db4da556f8e4dfe2197c73b28
mmiradev <mmirgal868@g.educaand.es>
2024-01-30 13:01:29 +0100

    created repository for Label

---
+++ src/Repository/LabelRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Label;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class LabelRepository extends ServiceEntityRepository
+{
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Label::class);
+    }
+}
